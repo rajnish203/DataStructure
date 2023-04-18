efe62b89df98a6d2f0f751e8c4c0e5d7667369a4
vi392 <rajnishdivakar618@gmail.com>
2023-04-18 11:08:25 +0530

    new File added

---
+++ DP/stair_case.java
@@ -0,0 +1,24 @@
+public class stair_case {
+    public static long All_possible_Ways(int n){
+        if(n==1 || n==0){
+            return n;
+        }
+        if(n==2){
+            return 2;
+        }
+        long dp[]=new long [n+1];
+        dp[0]=1;
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+        }
+        return dp[n];
+    }
+    public static void main(String[] args) {
+        int n=4;
+        long ans=All_possible_Ways(n);
+        System.out.print(ans);
+
+    }
+}
